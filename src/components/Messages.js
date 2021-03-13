8ab5c2d2c437230c338e7a773a4e60cf21d43ce3
yoshiki <katoyoshi9814@gmail.com>
2021-03-13 12:22:13 +0900

    削除機能の完成

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,7 +1,19 @@
-const Messages = ({ user, message }) => {
+import firebase from '../config/firebase'
+
+
+const Messages = ({ user, message, id }) => {
+    const handleDelete = (e) => {
+        e.preventDefault();
+        firebase.firestore().collection("message").doc(id).delete().then(() => {
+            console.log("Document successfully deleted!");
+        }).catch((error) => {
+            console.error("Error removing document: ", error);
+        });
+    }
+    
     return (
         <li>{user} : {message}
-            <button>削除</button></li>
+            <button onClick={(handleDelete)}>削除</button></li>
     )
 }
 
